5963e3c42493e9f7bb99e82062ffb152887185f8
13lheytens <lukeheytens@gmail.com>
2022-03-11 15:37:13 -0800

    adding comment to describe why create_voc_csvs.py was needed.

--- luke_src/create_voc_csvs.py
+++ luke_src/create_voc_csvs.py
@@ -1,6 +1,11 @@
 import dill
 import pandas as pd
 
+# @heytens: On my laptop, I was having issues loading the `voc_final.pkl` file. The dill package was throwing an
+#           exception as it loaded the binary data (only for the `voc_final.pkl` file). After spending too much time
+#           trying to fix it, I decided to use an AWS server to generate voc CSVs instead.
+#
+#           This file creates 3 voc csvs from the binary data in `voc_final.pkl`.
 def main():
     # load data
     data_dir = '../data/'
