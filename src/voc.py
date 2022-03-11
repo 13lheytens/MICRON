69a780c7618c30343c974b6ac7848696958277b3
13lheytens <lukeheytens@gmail.com>
2022-03-11 15:07:05 -0800

    Updating MICRON to use cpu and voc CSVs that I generated.

---
+++ src/voc.py
@@ -0,0 +1,13 @@
+# @heytens: I copied this from ../data/preprocessing.py to be used in MICRON.py.
+
+##### indexing file and final record
+class Voc(object):
+    def __init__(self):
+        self.idx2word = {}
+        self.word2idx = {}
+
+    def add_sentence(self, sentence):
+        for word in sentence:
+            if word not in self.word2idx:
+                self.idx2word[len(self.word2idx)] = word
+                self.word2idx[word] = len(self.word2idx)
